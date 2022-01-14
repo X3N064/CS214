431ad8f1d10aae5816441bf1919f84a5f648b61e
hs59 <hs59@remotel1.cs.calvin.edu>
2022-01-13 21:35:17 -0500

    Jan/13/2022

---
+++ lab/01/script.ada
@@ -0,0 +1,92 @@
+Script started on 2022-01-13 13:01:54-05:00 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="80" LINES="24"]
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ cat circle_area.adb
+-- circle_area.adb computes the area of a circle.
+--
+-- Input: The radius of the circle.
+-- Precondition: The radius is a positive number.
+-- Output: The area of the circle.
+--
+-- Begun by: Prof. Adams, CS 214 at Calvin College.
+-- Student name : Hyun Sim
+-- Completed by: 14 Jan, 2022
+-- Date:13 Jan, 2022
+----------------------------------------------------
+
+with Ada.Text_IO, Ada.Float_Text_IO;
+use Ada.Text_IO, Ada.Float_Text_IO;
+
+procedure circle_area is
+
+    radius, area : Float;
+
+    -- function circleArea computes a circle's area, given its radius
+    -- Parameter: r, a float
+    -- Precondition: r >= 0.0
+    -- Return: the area of the circle whose radius is r
+    ----------------------------------------------------
+    function circleArea (r : in Float) return Float is
+        PI : constant := 3.141_592_7;
+    begin
+        return PI * r**2;
+    end circleArea;
+
+begin
+    New_Line;
+    Put_Line ("To compute the area of a circle,");
+    Put ("enter its radius: ");
+    Get (radius);
+
+    area := circleArea (radius);
+
+    New_Line;
+    Put ("The circle's area is ");
+    Put (area);
+    New_Line;
+    New_Line;
+
+    Put ("The circle's area is ");
+    Put (area, 1, 15, 0);
+    New_Line;
+    New_Line;
+end circle_area;
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ gnatmake cior    circle_area.adb      
+gnatmake: "circle_area" up to date.
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ ./ciur  rcle_area  .adb    
+
+To compute the area of a circle,
+enter its radius: 1
+
+The circle's area is  3.14159E+00
+
+The circle's area is 3.141592741012573
+
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ ./circle_area
+
+To compute the area of a circle,
+enter its radius: 2
+
+The circle's area is  1.25664E+01
+
+The circle's area is 12.566370964050293
+
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ ./circle_area
+
+To compute the area of a circle,
+enter its radius: 2.5
+
+The circle's area is  1.96350E+01
+
+The circle's area is 19.634954452514648
+
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ ./circle_area
+
+To compute the area of a circle,
+enter its radius: 4.99999
+
+The circle's area is  7.85395E+01
+
+The circle's area is 78.539505004882813
+
+]0;hs59@gold31: ~/cs214/lab01/ada[01;32mhs59@gold31[00m:[01;34m~/cs214/lab01/ada[00m$ exit
+
+Script done on 2022-01-13 13:02:36-05:00 [COMMAND_EXIT_CODE="0"]
---
+++ lab/01/script.java
@@ -0,0 +1,98 @@
+Script started on 2022-01-13 20:25:47-05:00 [TERM="xterm-256color" TTY="/dev/pts/0" COLUMNS="80" LINES="24"]
+]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ cat ciircle     rcle      Cri  ircleArea.java
+/* CircleArea.java computes the area of a circle.
+ *
+ * Input: The radius of the circle.
+ * Precondition: The radius is a positive number.
+ * Output: The area of the circle.
+ *
+ * Begun by: Prof. Adams, CS 214 at Calvin College.
+ * Student name : Hyun Sim
+ * Completed by: 14 Jan, 2022
+ * Date:13 Jan, 2022
+ **********************************************************/
+
+import java.util.Scanner; // include class for Scanner
+
+public class CircleArea {
+
+   /*
+    * function circleArea() computes a circle's area, given its radius.
+    * Parameter: r, a double
+    * Precondition: r is not negative.
+    * Returns: the area of the circle
+    */
+   public static double circleArea(double r) {
+      return Math.PI * r * r; // return an expression
+   } // circleArea method
+
+   // main program
+   public static void main(String[] args) {
+      // prompt for radius
+      System.out.println("\n\nTo compute the area of a circle,");
+      System.out.print(" enter its radius: ");
+
+      // Create a connection named keyboard to standard in
+      Scanner keyboard = new Scanner(System.in);
+
+      // Get the number from the user
+      double radius = keyboard.nextDouble();
+
+      // output area
+      System.out.println("\nThe area is " + circleArea(radius) + "\n");
+      System.out.printf("The area is %f\n\n", circleArea(radius));
+      System.out.printf("The area is %.15f\n\n", circleArea(radius));
+   } // main method
+
+} // class CircleArea]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ javac -deprecation CircleArea.java 
+]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ java CircleArea 
+
+
+To compute the area of a circle,
+ enter its radius: 1
+
+The area is 3.141592653589793
+
+The area is 3.141593
+
+The area is 3.141592653589793
+
+]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ java CircleArea 
+
+
+To compute the area of a circle,
+ enter its radius: 2
+
+The area is 12.566370614359172
+
+The area is 12.566371
+
+The area is 12.566370614359172
+
+]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ java CircleArea 
+
+
+To compute the area of a circle,
+ enter its radius: 2.5
+
+The area is 19.634954084936208
+
+The area is 19.634954
+
+The area is 19.634954084936208
+
+]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ java CircleArea 
+
+
+To compute the area of a circle,
+ enter its radius: 4.99999
+
+The area is 78.53950218079365
+
+The area is 78.539502
+
+The area is 78.539502180793650
+
+]0;hs59@remotel2: ~/cs214/lab/01/java[01;32mhs59@remotel2[00m:[01;34m~/cs214/lab/01/java[00m$ exit
+
+Script done on 2022-01-13 20:26:46-05:00 [COMMAND_EXIT_CODE="0"]
